f872b6f7cdf36590786859e5bb87d7142e029fd5
axell-brendow <breno.axel@gmail.com>
2019-08-10 19:58:40 -0300

    Ajuste de legibilidade na aula 4

--- Python 3 - Prof. Luiz Otávio/aula4-tipos-primitivos/aula4.py
+++ Python 3 - Prof. Luiz Otávio/aula4-tipos-primitivos/aula4.py
@@ -5,11 +5,12 @@ int - inteiro - 123456 0 -10 -20 -50 -60 -15000 1500
 float - real/ponto flutuante - 10.50 1.5 -10.10 -50.93 0.0
 bool - booleano/lógico - True/False - 10 == 10
 """
-print('Luiz'  , type('Luiz')                    )
-print(10      , type(10)                        )
-print(25.23   , type(25.23)                     )
-print(10 == 10, type(10 == 10)                  )
-print('10'    , type('10')    , type(int('10')) )  # Converte str '10' para int 10
+
+print('Luiz'   , type('Luiz')                    )
+print(10       , type(10)                        )
+print(25.23    , type(25.23)                     )
+print(10 == 10 , type(10 == 10)                  )
+print('10'     , type('10')    , type(int('10')) )  # Converte str '10' para int 10
 
 # Dados
 # nome: String
