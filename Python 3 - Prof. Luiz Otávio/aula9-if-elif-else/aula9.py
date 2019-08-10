f24b2c4d95e76575dac5552ed66bcf2e39e21caa
axell-brendow <breno.axel@gmail.com>
2019-08-10 16:41:55 -0300

    Upload aula9-if-elif-else

---
+++ Python 3 - Prof. Luiz Otávio/aula9-if-elif-else/aula9.py
@@ -0,0 +1,18 @@
+"""
+Condições IF, ELIF e ELSE - Parte1
+"""
+
+if False:
+    print('Verdadeiro.')
+    print('teste teste2.')
+elif False:
+    print('Agora é verdadeiro.')
+    nome = input('Qual o seu nome ? ')
+
+    print(f'Seu nome é {nome}.')
+elif False:
+    print('Mais uma verdadeira.')
+    print(22 + 22)
+else:
+    print('Não é verdadeiro.')
+    print('Olá.')
