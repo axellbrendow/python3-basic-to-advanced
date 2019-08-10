c064696f9d2c6a64c3276f7fdafcb58aa2444399
axell-brendow <breno.axel@gmail.com>
2019-08-10 16:57:12 -0300

    Upload aula10-operadores-relacionais

---
+++ Python 3 - Prof. Luiz Otávio/aula10-operadores-relacionais/aula10.py
@@ -0,0 +1,37 @@
+"""
+Operadores relacionais
+== > >= < <= !=
+"""
+
+value1 = 2
+value2 = '2'
+value3 = (value1 == value2)  # False
+
+print(value3)
+
+value2 = 2
+value3 = (value1 > value2)
+print(value3)
+
+value3 = (value1 >= value2)
+print(value3)
+
+value3 = (value1 < value2)
+print(value3)
+
+value3 = (value1 <= value2)
+print(value3)
+
+value3 = (value1 != value2)
+print(value3)
+
+nome = input('Qual é o seu nome ? ')
+idade = int(input('Qual a sua idade ? '))
+
+idade_menor = 20
+idade_maior = 30
+
+if (idade >= idade_menor and idade <= idade_maior):
+    print(f'{nome} pode pegar o empréstimo.')
+else:
+    print(f'{nome} NÃO pode pegar o empréstimo.')
