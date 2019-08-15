82137ff85ecba68e5bc72209013f04ca215c0a93
axell-brendow <breno.axel@gmail.com>
2019-08-15 15:49:25 -0300

    Volta alguns arquivos excluídos

---
+++ Python 3 - Prof. Luiz Otávio/aula03-str-e-aspas/aula3.py
@@ -0,0 +1,10 @@
+"""
+str - string
+"""
+
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+print("Essa é uma \"string\" (str).")
+print('Essa é uma \'string\' (str).')
+print(r'Essa é uma string bruta (raw) \n (str).')
+print('Essa é uma quebra de linha \n (str).')
--- Python 3 - Prof. Luiz Otávio/aula10-operadores-relacionais/aula10.py
+++ Python 3 - Prof. Luiz Otávio/aula10-operadores-relacionais/aula10.py
@@ -31,7 +31,10 @@ idade = int(input('Qual a sua idade ? '))
 idade_menor = 20
 idade_maior = 30
 
-if (idade >= idade_menor and idade <= idade_maior):
+if idade_menor <= idade <= idade_maior:
     print(f'{nome} pode pegar o empréstimo.')
+
 else:
     print(f'{nome} NÃO pode pegar o empréstimo.')
+    print(f'Idade mínima: {idade_menor}.')
+    print(f'Idade máxima: {idade_maior}.')
