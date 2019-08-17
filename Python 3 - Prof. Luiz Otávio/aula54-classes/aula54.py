2bf7b79451dff7aa039e82a4140795f6a2a3f728
axell-brendow <breno.axel@gmail.com>
2019-08-17 10:26:44 -0300

    Upload aula54-classes

--- Python 3 - Prof. Luiz Otávio/aula54-classes/aula54.py
+++ Python 3 - Prof. Luiz Otávio/aula54-classes/aula54.py
@@ -1,5 +1,19 @@
 from pessoa import Pessoa
 
 p1 = Pessoa('Luiz', 20)
+p2 = Pessoa('Maria', 19)
 
 p1.comer('maçã')
+p1.comer('pera')
+p1.parar_de_comer()
+p1.parar_de_comer()
+p1.comer('pera')
+p1.parar_de_comer()
+p1.falar('Python')
+p1.falar('C++')
+p1.parar_de_falar()
+p1.falar('C++')
+
+print(p1.ano_atual)
+print(Pessoa.ano_atual)
+print(p1.ano_de_nascimento())
