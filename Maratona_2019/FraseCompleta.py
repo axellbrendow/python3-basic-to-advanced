39844556df71d3b6f3b294e6f34ac22cca3978f3
axell-brendow <breno.axel@gmail.com>
2019-08-18 13:36:29 -0300

    Upload desafio FraseCompleta

---
+++ Maratona_2019/FraseCompleta.py
@@ -0,0 +1,15 @@
+TAMANHO_ALFABETO = 26
+numero_de_entradas = int(input())
+
+for i in range(numero_de_entradas):
+    frase = input()
+    numero_de_letras = len({c for c in frase if 'a' <= c <= 'z'})
+
+    if numero_de_letras >= TAMANHO_ALFABETO:
+        print('frase completa')
+
+    elif numero_de_letras >= TAMANHO_ALFABETO / 2:
+        print('frase quase completa')
+
+    else:
+        print('frase mal elaborada')
