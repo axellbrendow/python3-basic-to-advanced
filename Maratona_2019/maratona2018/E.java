50ab03d2d679dc2aceb789184d4c9cd7ff634509
axell-brendow <breno.axel@gmail.com>
2019-09-09 17:28:59 -0300

    Começa o pré-processamento do algoritmo de Boyer Moore

--- Maratona_2019/maratona2018/E.java
+++ Maratona_2019/maratona2018/E.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class E
 {
@@ -7,9 +8,21 @@ public class E
 		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 	};
 	
+	public static final int TAMANHO_ALFABETO = 26;
+	public static final int[] deslocamentos = new int[TAMANHO_ALFABETO];
+	public static int hash(char c) { return c - 'A'; }
+	
 	public static void main(String[] args)
 	{
 		String mensagemCifrada = IO.readLine();
 		String crib = IO.readLine();
+		int tamanhoCrib = crib.length();
+		
+		Arrays.fill(deslocamentos, tamanhoCrib); // Coloca o tamanho do crib em todas as posições
+		
+		for (int i = 0; i < tamanhoCrib; i++)
+		{
+			deslocamentos[hash(crib.charAt(i))] = tamanhoCrib - 1 - i;
+		}
 	}
 }
