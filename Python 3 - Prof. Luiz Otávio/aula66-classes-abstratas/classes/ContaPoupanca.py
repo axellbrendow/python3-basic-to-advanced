ed1cc1fcd936c227c79b2fde05ff926622715840
axell-brendow <breno.axel@gmail.com>
2019-12-16 06:08:24 -0300

    Upload aula66-classes-abstratas

---
+++ Python 3 - Prof. Luiz Otávio/aula66-classes-abstratas/classes/ContaPoupanca.py
@@ -0,0 +1,12 @@
+from classes.conta import Conta
+
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        if self.saldo < valor:
+            print('Saldo insuficiente')
+
+        else:
+            self.saldo -= valor
+            print(f'Você sacou {valor} reais')
+            self.detalhes()
