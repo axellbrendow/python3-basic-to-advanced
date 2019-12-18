516b10c23b850bd83ea23331d263a9428070e8cb
axell-brendow <breno.axel@gmail.com>
2019-12-18 10:57:38 -0300

    Upload aula86-web-scraping

---
+++ Python 3 - Prof. Luiz Otávio/aula86-web-scraping/aula86.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://pt.stackoverflow.com/questions/tagged/python'
+response = requests.get(url)
+html = BeautifulSoup(response.text, 'html.parser')
+
+for pergunta in html.select('.question-summary'):
+    titulo = pergunta.select_one('.question-hyperlink')
+    data = pergunta.select_one('.relativetime')
+    votos = pergunta.select_one('.vote-count-post')
+    print(titulo.text)
+    print(data.text)
+    print(votos.text)
+    print()
