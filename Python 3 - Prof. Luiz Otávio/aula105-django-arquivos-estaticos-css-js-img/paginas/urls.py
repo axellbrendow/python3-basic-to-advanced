5b1271f8a5474772f204bbfa6e5a7bf50004be85
axell-brendow <breno.axel@gmail.com>
2020-01-02 18:40:06 -0300

    Correção aula105-django-arquivos-estaticos-css-js-img

--- Python 3 - Prof. Luiz Otávio/aula105-django-arquivos-estaticos-css-js-img/paginas/urls.py
+++ Python 3 - Prof. Luiz Otávio/aula105-django-arquivos-estaticos-css-js-img/paginas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('sobre/', views.sobre),
+    path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
 ]
