af31b6d859435ad600cd2c1217d1e253196dbe96
axell-brendow <breno.axel@gmail.com>
2020-01-02 20:20:22 -0300

    Upload aula107ate108-django-projeto-agenda

---
+++ Python 3 - Prof. Luiz Otávio/aula107ate108-django-projeto-agenda/contatos/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
