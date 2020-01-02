92575d2ba6c1c64ce563654e8b52feed60c4acba
axell-brendow <breno.axel@gmail.com>
2020-01-02 20:45:49 -0300

    Upload aula107ate109-django-projeto-agenda

---
+++ Python 3 - Prof. Luiz Otávio/aula107ate109-django-projeto-agenda/contatos/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Categoria, Contato
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('nome', 'sobrenome')
+    list_per_page = 1
+    search_fields = ('nome', 'sobrenome', 'telefone')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- Python 3 - Prof. Luiz Otávio/aula107ate109-django-projeto-agenda/contatos/models.py
+++ Python 3 - Prof. Luiz Otávio/aula107ate109-django-projeto-agenda/contatos/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
@@ -14,3 +17,6 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
