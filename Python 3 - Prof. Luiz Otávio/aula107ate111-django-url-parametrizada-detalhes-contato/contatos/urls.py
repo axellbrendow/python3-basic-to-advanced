083e8ce6e1f2f7afe555cd4822df558e6fb9bd77
axell-brendow <breno.axel@gmail.com>
2020-01-02 23:53:48 -0300

    Upload aula107ate111-django-url-parametrizada-detalhes-contato

--- Python 3 - Prof. Luiz Otávio/aula107ate111-django-url-parametrizada-detalhes-contato/contatos/urls.py
+++ Python 3 - Prof. Luiz Otávio/aula107ate111-django-url-parametrizada-detalhes-contato/contatos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:contato_id>', views.ver_contato, name='ver_contato'),
 ]
--- Python 3 - Prof. Luiz Otávio/aula107ate111-django-url-parametrizada-detalhes-contato/contatos/views.py
+++ Python 3 - Prof. Luiz Otávio/aula107ate111-django-url-parametrizada-detalhes-contato/contatos/views.py
@@ -8,3 +8,11 @@ def index(request):
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
+
+
+def ver_contato(request, contato_id):
+    contato = Contato.objects.get(id=contato_id)
+
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
