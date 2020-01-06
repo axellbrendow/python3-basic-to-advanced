3bb6ee574b65370034666adbf37115de00ff77ce
axell-brendow <breno.axel@gmail.com>
2020-01-05 22:57:57 -0300

    Upload aula107ate114-django-paginacao

--- Python 3 - Prof. Luiz Otávio/aula107ate114-django-paginacao/contatos/views.py
+++ Python 3 - Prof. Luiz Otávio/aula107ate114-django-paginacao/contatos/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Contato
 
 
 def index(request):
     contatos = Contato.objects.all()  # Faz um select em todos os contatos
+    paginator = Paginator(contatos, 20)
+
+    page = request.GET.get('page')
+    contatos = paginator.get_page(page)
 
     return render(request, 'contatos/index.html', {
         'contatos': contatos
