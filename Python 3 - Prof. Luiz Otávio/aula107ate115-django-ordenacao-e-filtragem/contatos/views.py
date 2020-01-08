f96b920a0d696b4a74873713b163a8ea2f7498a4
axell-brendow <breno.axel@gmail.com>
2020-01-08 09:09:21 -0300

    Upload aula107ate115-django-ordenacao-e-filtragem

--- Python 3 - Prof. Luiz Otávio/aula107ate115-django-ordenacao-e-filtragem/contatos/views.py
+++ Python 3 - Prof. Luiz Otávio/aula107ate115-django-ordenacao-e-filtragem/contatos/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.core.paginator import Paginator
 from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()  # Faz um select em todos os contatos
+    # Faz a busca dos Contatos na base de dados
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 20)
 
     page = request.GET.get('page')
@@ -18,6 +22,9 @@ def index(request):
 def ver_contato(request, contato_id):
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
