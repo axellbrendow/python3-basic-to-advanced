3472066ea0177e4d06fd55dc648305997d002c8e
axell-brendow <breno.axel@gmail.com>
2020-01-09 23:26:02 -0300

    Upload aula107ate116-django-campo-de-pesquisa

--- Python 3 - Prof. Luiz Otávio/aula107ate116-django-campo-de-pesquisa/contatos/urls.py
+++ Python 3 - Prof. Luiz Otávio/aula107ate116-django-campo-de-pesquisa/contatos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('buscar/', views.buscar, name='buscar'),
     path('<int:contato_id>', views.ver_contato, name='ver_contato'),
 ]
