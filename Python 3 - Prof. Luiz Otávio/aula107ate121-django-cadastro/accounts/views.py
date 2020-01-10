8d7b14448c63cded624c02b68b1779d12c7a4de9
axell-brendow <breno.axel@gmail.com>
2020-01-10 08:31:34 -0300

    Upload do início da aula107ate121-django-cadastro

---
+++ Python 3 - Prof. Luiz Otávio/aula107ate121-django-cadastro/accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.contrib import messages
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
+
+
+def register(request):
+    response = render(request, 'accounts/register.html')
+
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        usuario = request.POST.get('usuario')
+        password = request.POST.get('password')
+        passwordConfirm = request.POST.get('password-confirm')
+
+        if not nome or not sobrenome or not email or not usuario or not password or\
+                not passwordConfirm:
+            print('entrou nas nots')
+            messages.add_message(request, messages.ERROR, 'Nenhum campo pode estar vazio')
+            response = render(request, 'accounts/register.html')
+
+        else:
+            print('passou das nots')
+
+    return response
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
