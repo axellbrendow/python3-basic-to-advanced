1861063375252659da5d83b494980d431c09c59e
axell-brendow <breno.axel@gmail.com>
2020-01-21 15:18:30 -0300

    Implementa mais validações e redireciona para o login após cadastro

--- Python 3 - Prof. Luiz Otávio/aula107ate121-django-cadastro/accounts/views.py
+++ Python 3 - Prof. Luiz Otávio/aula107ate121-django-cadastro/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.validators import validate_email
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -24,23 +25,43 @@ def register(request):
                 not passwordConfirm:
             messages.add_message(request, messages.ERROR, 'Nenhum campo pode estar vazio')
 
+        try:
+            validate_email(email)
+
+        except:
+            messages.add_message(request, messages.ERROR, 'Email inválido')
+
+        if User.objects.filter(email=email).exists():
+            messages.add_message(request, messages.ERROR, 'E-mail já existe')
+
+        if len(usuario) < 6:
+            messages.add_message(request, messages.ERROR,
+                                 'Usuário precisa ter 6 ou mais caracteres')
+
+        if User.objects.filter(username=usuario).exists():
+            messages.add_message(request, messages.ERROR, 'Usuário já existe')
+
+        if len(password) < 6:
+            messages.add_message(request, messages.ERROR,
+                                 'Senha precisa ter 6 ou mais caracteres')
+
+        if password != passwordConfirm:
+            messages.add_message(request, messages.ERROR, 'Senhas não conferem')
+
+        if len(messages.get_messages(request)):  # Havendo alguma mensagem de erro,
+            response = render(request, 'accounts/register.html')  # Volta ao registro
+
         else:
-            try:
-                validate_email(email)
+            messages.add_message(request, messages.SUCCESS, 'Usuário registrado com sucesso')
 
-            except:
-                messages.add_message(request, messages.ERROR, 'Email inválido')
+            user = User.objects.create_user(username=usuario, email=email,
+                                            password=password, first_name=nome,
+                                            last_name=sobrenome)
+            user.save()
+            response = redirect('login')
 
-            if len(password) < 6:
-                messages.add_message(request, messages.ERROR, 'Senha precisa ter 6 ou mais caracteres')
-
-            if len(passwordConfirm) < 6:
-                messages.add_message(request, messages.ERROR, 'Senha precisa ter 6 ou mais caracteres')
-
-            if password != passwordConfirm:
-                messages.add_message(request, messages.ERROR, 'Senhas não conferem')
-
-    response = render(request, 'accounts/register.html')
+    else:
+        response = render(request, 'accounts/register.html')
 
     return response
 
