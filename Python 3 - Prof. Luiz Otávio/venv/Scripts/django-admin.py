fbb788ffcd8120e2aee562024d2eaa6252f6b0c8
axell-brendow <breno.axel@gmail.com>
2020-02-03 19:21:25 -0300

    Upload aula139ate142-ecommerce-slug-automatico-e-cadastro-de-produtos

---
+++ Python 3 - Prof. Luiz Otávio/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\1137910\downloads\cursospython\python 3 - prof. luiz otávio\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
